fee26285897f873acaabd252a7c7ab88d0626d34
chugumba <chugumba@rambler.ru>
2025-01-22 23:12:13 +0300

    Получаем имена на Остановок на сервере

--- api/find-bus.php
+++ api/find-bus.php
@@ -4,14 +4,16 @@ require_once '../DBcontroller.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $start = $_POST['start'];
     $stop = $_POST['stop'];
+    $startName = $_POST['startName'];
+    $stopName = $_POST['stopName'];
     
-    if (empty($start) || empty($stop)) {
+    if (empty($start) || empty($stop) || empty($startName) || empty($stopName)) {
         echo 'Both starting and ending stops are required.';
         exit;
     }
 
     $db = new dbController();
-    $res = $db->getRoute($start, $stop);
+    $res = $db->getRoute($start, $stop, $startName, $stopName);
 
     echo $res;
 }
